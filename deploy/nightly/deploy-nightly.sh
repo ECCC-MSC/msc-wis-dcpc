b4b0958a6c50794f0f9733b0fd77f72c5757dec7
Tom Kralidis <tomkralidis@gmail.com>
2021-10-01 19:46:40 +0000

    fix refs

--- deploy/nightly/deploy-nightly.sh
+++ deploy/nightly/deploy-nightly.sh
@@ -79,8 +79,8 @@ echo "Generating metadata repository"
 rm -fr /tmp/discovery-metadata-master.zip /tmp/discovery-metadata-master/
 
 mkdir data
-curl -o /tmp/disovery-metadata-master.zip $MSC_DISCOVERY_METADATA_REPO
-unzip /tmp/disovery-metadata-master.zip -d /tmp
+curl -o /tmp/discovery-metadata-master.zip $MSC_DISCOVERY_METADATA_REPO
+unzip /tmp/discovery-metadata-master.zip -d /tmp
 pycsw-admin.py setup-db -c msc-wis-dcpc/deploy/nightly/msc-wis-dcpc-pycsw-config.cfg
 pycsw-admin.py load-records -c msc-wis-dcpc/deploy/nightly/msc-wis-dcpc-pycsw-config.cfg -p /tmp/discovery-metadata-master/legacy/wis/records -r -y
 msc-wis-dcpc metadata add --type OGC:CSW --url https://geo.woudc.org/csw
